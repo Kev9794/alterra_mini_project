d1f240d0b1192763a9c3802ca3c1926c4b0e4840
Kev9794 <k.natanael88@gmail.com>
2023-04-22 22:02:19 +0700

    add assets and on boarding page

--- lib/views/screens/on_boarding_page.dart
+++ lib/views/screens/on_boarding_page.dart
@@ -1,5 +1,9 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import "package:introduction_screen/introduction_screen.dart";
 
 class OnBoardingPage extends StatefulWidget {
   const OnBoardingPage({super.key});
@@ -9,8 +13,105 @@ class OnBoardingPage extends StatefulWidget {
 }
 
 class _OnBoardingPageState extends State<OnBoardingPage> {
+  final introKey = GlobalKey<IntroductionScreenState>();
+
+  void _onIntroEnd(context) {
+    Navigator.pushReplacementNamed(context, '/list');
+  }
+
+  Widget _buildImage(String assetName, [double width = 300]) {
+    return Image.asset('assets/img/$assetName', width: width);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    const bodyStyle = TextStyle(fontSize: 19.0);
+
+    const pageDecoration = PageDecoration(
+      titleTextStyle: TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700),
+      bodyTextStyle: bodyStyle,
+      bodyPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+      pageColor: Colors.white,
+      imagePadding: EdgeInsets.zero,
+    );
+
+    return IntroductionScreen(
+      key: introKey,
+      globalBackgroundColor: Colors.white,
+      pages: [
+        PageViewModel(
+          title: "Selamat Datang di Warehouse App",
+          body: "Catat seluruh stok anda hanya dalam satu aplikasi",
+          image: _buildImage('ob1.jpg'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Mudah dikelola",
+          body: "Mengatur seluruh stok anda dimana saja dan kapan saja",
+          image: Center(
+            child: _buildImage('ob2.jpg'),
+          ),
+          decoration: pageDecoration.copyWith(
+            imagePadding: const EdgeInsets.only(top: 130),
+            contentMargin: const EdgeInsets.only(top: 70),
+            imageAlignment: Alignment.bottomCenter,
+          ),
+        ),
+        PageViewModel(
+          title: "Mendata seluruh gudang anda",
+          body:
+              "Catat seluruh stok dari semua gudang supermarket secara terpusat",
+          image: _buildImage('ob3.jpg'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Warehouse App",
+          bodyWidget: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text("Tidak perlu menunggu lama lagi", style: bodyStyle),
+            ],
+          ),
+          decoration: pageDecoration.copyWith(
+            bodyFlex: 2,
+            imageFlex: 4,
+            contentMargin: const EdgeInsets.only(top: 50),
+            bodyAlignment: Alignment.bottomCenter,
+            imageAlignment: Alignment.topCenter,
+          ),
+          image: Image.asset('assets/logo/logo.png', width: 300),
+          reverse: true,
+        ),
+      ],
+      onDone: () => _onIntroEnd(context),
+      //onSkip: () => _onIntroEnd(context), // You can override onSkip callback
+      showSkipButton: false,
+      skipOrBackFlex: 0,
+      nextFlex: 0,
+      showBackButton: true,
+      //rtl: true, // Display as right-to-left
+      back: const Icon(Icons.arrow_back),
+      next: const Icon(Icons.arrow_forward),
+      done: const Text('Mulai', style: TextStyle(fontWeight: FontWeight.w600)),
+      curve: Curves.fastLinearToSlowEaseIn,
+      controlsMargin: const EdgeInsets.all(16),
+      controlsPadding: kIsWeb
+          ? const EdgeInsets.all(12.0)
+          : const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
+      dotsDecorator: const DotsDecorator(
+        size: Size(10.0, 10.0),
+        color: Color(0xFFBDBDBD),
+        activeSize: Size(22.0, 10.0),
+        activeShape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(25.0)),
+        ),
+      ),
+      dotsContainerDecorator: const ShapeDecoration(
+        color: Colors.black87,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(8.0)),
+        ),
+      ),
+    );
   }
 }
