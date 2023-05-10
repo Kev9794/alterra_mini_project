ff16ace1dc44e6cc96d46358cf496ee11051ddbd
Kev9794 <k.natanael88@gmail.com>
2023-05-10 22:24:22 +0700

    update unit testing

--- test/firestore_services_test.dart
+++ test/firestore_services_test.dart
@@ -3,7 +3,7 @@ import 'package:flutter/foundation.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('firestore testing', () async {
+  test('firestore add', () async {
     final instance = FakeFirebaseFirestore();
     await instance.collection('barangs').add({
       'nama': 'Barang Test',
@@ -12,6 +12,27 @@ void main() {
       'deskripsi': 'deskripsi test',
       'stok': int.tryParse('5') ?? 0,
     });
+    final snapshot = await instance.collection('barangs').get();
+    if (kDebugMode) {
+      print(snapshot.docs.length);
+    }
+    if (kDebugMode) {
+      print(snapshot.docs.first.get('nama'));
+    }
+    if (kDebugMode) {
+      print(instance.dump());
+    }
+  });
+  test('firestore delete', () async {
+    final instance = FakeFirebaseFirestore();
+    await instance.collection('barangs').add({
+      'nama': 'Barang Test',
+      'tempat': 'Malang',
+      'harga': int.tryParse('20') ?? 0,
+      'deskripsi': 'deskripsi test',
+      'stok': int.tryParse('5') ?? 0,
+    });
+    await instance.collection('barangs').doc().delete();
     final snapshot = await instance.collection('barangs').get();
     if (kDebugMode) {
       print(snapshot.docs.length);
